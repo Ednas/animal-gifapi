4392b3b446c6e1ecb2400d3206b46af13ee31473
Ednas <edna@edna.tech>
2016-09-15 23:25:31 -0400

    moved app.js to right place and started adding the array

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(function() {
+	    populateButtons(animals, 'animalButton', '#animalButtons');
+
+});
+
+
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+
+//function to make buttons and add to page
+
+
+
+$('.animalButton')
