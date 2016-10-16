407dc9ce3aff44b9cd478bba3bd45e986cacfe4a
Ednas <edna@edna.tech>
2016-10-16 19:05:14 -0400

    Added bootstrap, made the app more spaced out, easier to look at, added bootstrap gridding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,10 @@ $(function() {
 });
 
 
-var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", 
+"bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", 
+"hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", 
+"serval", "salamander", "frog"];
 
 
 //function to make buttons and add to page
